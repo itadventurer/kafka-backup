b4d8ecd39d55a1d23ca3225799176b98ab317afb
Anatoly Zelenin <anatoly@zelenin.de>
2020-01-21 16:42:13 +0100

    Upgraded dependencies + Cleanup of Test Scripts

--- docs/Build_and_Run.md
+++ docs/Build_and_Run.md
@@ -2,9 +2,22 @@
 
 ## Build Kafka with the API Changes required for `kafka-backup` restore
 
+Just clone Ryanne Dolan's Kafka repository containing the source code
+for Mirror Maker 2 .
+
+```sh
+git clone https://github.com/ryannedolan/kafka.git
+git checkout KIP-382
+```
+
+
+
 ## Build `kafka-backup`
 
 You should be able to build this with `./gradlew shadowJar`. Once the jar is generated in `build/libs`, include it in `CLASSPATH` (e.g., `export CLASSPATH=.:$CLASSPATH:/fullpath/to/kafka-backup-jar` )
 
 Run: `connect-standalone example-connect-worker.properties
 example-kafka-backup-sink.properties`
+
+
+## Docker
--- system_test/full_test/round_trip.sh
+++ system_test/full_test/round_trip.sh
@@ -23,30 +23,30 @@ create_topic backup-test-3partitions 3
 #
 # We need to chunk the production of messages as otherwise we cannot
 # guarantee that the group consumer will evenly consume the partitions.
-produce_messages backup-test-1partition 0 0 $((3 * $NUM_MSG))
+produce_messages backup-test-1partition 0 0 $((3 * NUM_MSG))
 # backup-test-3partition
-produce_messages backup-test-3partitions 0 0 $((3 * $NUM_MSG))
-produce_messages backup-test-3partitions 1 0 $((3 * $NUM_MSG))
-produce_messages backup-test-3partitions 2 0 $((3 * $NUM_MSG))
+produce_messages backup-test-3partitions 0 0 $((3 * NUM_MSG))
+produce_messages backup-test-3partitions 1 0 $((3 * NUM_MSG))
+produce_messages backup-test-3partitions 2 0 $((3 * NUM_MSG))
 # * Consume all messages with consumer-group `cg-3k`
-consume_messages backup-test-1partition cg-3k $((3 * $NUM_MSG))
-consume_messages backup-test-3partitions cg-3k $((9 * $NUM_MSG))
+consume_messages backup-test-1partition cg-3k $((3 * NUM_MSG))
+consume_messages backup-test-3partitions cg-3k $((9 * NUM_MSG))
 kafka_group_describe cg-3k
 # * Produce 2 * NUM_MSG messages
-produce_messages backup-test-1partition 0 $((3 * $NUM_MSG)) $((2 * $NUM_MSG))
+produce_messages backup-test-1partition 0 $((3 * NUM_MSG)) $((2 * NUM_MSG))
 # backup-test-3partition
-produce_messages backup-test-3partitions 0 $((3 * $NUM_MSG)) $((2 * $NUM_MSG))
-produce_messages backup-test-3partitions 1 $((3 * $NUM_MSG)) $((2 * $NUM_MSG))
-produce_messages backup-test-3partitions 2 $((3 * $NUM_MSG)) $((2 * $NUM_MSG))
+produce_messages backup-test-3partitions 0 $((3 * NUM_MSG)) $((2 * NUM_MSG))
+produce_messages backup-test-3partitions 1 $((3 * NUM_MSG)) $((2 * NUM_MSG))
+produce_messages backup-test-3partitions 2 $((3 * NUM_MSG)) $((2 * NUM_MSG))
 # * Consume all messages with consumer-group `cg-5k`
-consume_messages backup-test-1partition cg-5k $((5 * $NUM_MSG))
-consume_messages backup-test-3partitions cg-5k $((15 * $NUM_MSG))
+consume_messages backup-test-1partition cg-5k $((5 * NUM_MSG))
+consume_messages backup-test-3partitions cg-5k $((15 * NUM_MSG))
 # * Produce 100 more messages
-produce_messages backup-test-1partition 0 $((5 * $NUM_MSG)) $((1 * $NUM_MSG))
+produce_messages backup-test-1partition 0 $((5 * NUM_MSG)) $((1 * NUM_MSG))
 # backup-test-3partition
-produce_messages backup-test-3partitions 0 $((5 * $NUM_MSG)) $((1 * $NUM_MSG))
-produce_messages backup-test-3partitions 1 $((5 * $NUM_MSG)) $((1 * $NUM_MSG))
-produce_messages backup-test-3partitions 2 $((5 * $NUM_MSG)) $((1 * $NUM_MSG))
+produce_messages backup-test-3partitions 0 $((5 * NUM_MSG)) $((1 * NUM_MSG))
+produce_messages backup-test-3partitions 1 $((5 * NUM_MSG)) $((1 * NUM_MSG))
+produce_messages backup-test-3partitions 2 $((5 * NUM_MSG)) $((1 * NUM_MSG))
 # * Start Kafka Connect distributed
 kafka_connect_start
 # * Configure backup-sink:
@@ -61,10 +61,10 @@ sleep 10
 create_topic backup-test-10partitions 10
 # * Produce 1,00 messages as above and consume 500 messages as above
 for i in {0..9} ; do
-    produce_messages backup-test-10partitions $i 0 $((5 * $NUM_MSG))
+    produce_messages backup-test-10partitions $i 0 $((5 * NUM_MSG))
 done
 # To force segmentation rolling
-produce_messages backup-test-1partition 0 $((6 * $NUM_MSG)) $((15 * $NUM_MSG))
+produce_messages backup-test-1partition 0 $((6 * NUM_MSG)) $((15 * NUM_MSG))
 # Consume some messages
 # * Wait a few minutes
 sleep $((60*5))
@@ -88,10 +88,10 @@ kafka_connect_unload_connector backup-source
 # * Read all messages and check that they are the same as the ones that were written.
 # * Subscribe to Kafka using the consumer groups as above (`cg-5k` and `cg-3k`) and check whether they are at the correct position
 
-consume_verify_messages backup-test-1partition 0 $((21 * $NUM_MSG))
+consume_verify_messages backup-test-1partition 0 $((21 * NUM_MSG))
 for i in {0..2} ; do
-    consume_verify_messages backup-test-3partitions $i $((6 * $NUM_MSG))
+    consume_verify_messages backup-test-3partitions $i $((6 * NUM_MSG))
 done
 for i in {0..9} ; do
-    consume_verify_messages backup-test-10partitions $i $((5 * $NUM_MSG))
+    consume_verify_messages backup-test-10partitions $i $((5 * NUM_MSG))
 done
