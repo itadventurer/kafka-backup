8d07c3a287d9048129f5be022a3efbd900e6e95b
Anatoly Zelenin <anatoly@zelenin.de>
2020-06-21 22:01:48 +0200

    Improved printing of segment.sh

--- src/main/java/de/azapps/kafkabackup/cli/formatters/ListRecordFormatter.java
+++ src/main/java/de/azapps/kafkabackup/cli/formatters/ListRecordFormatter.java
@@ -7,7 +7,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class ListRecordFormatter extends RecordFormatter {
-    private DateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private final DateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public ListRecordFormatter(ByteFormatter keyFormatter, ByteFormatter valueFormatter) {
         super(keyFormatter, valueFormatter);
@@ -15,9 +15,36 @@ public class ListRecordFormatter extends RecordFormatter {
 
     @Override
     public void writeTo(Record record, PrintStream outputStream) {
-        outputStream.println("Offset: " + record.kafkaOffset()
-                + " Key: " + keyFormatter.toString(record.key())
-                + " Timestamp: " + timestampFormat.format(record.timestamp())
-                + " Data Length: " + record.value().length);
+        String offset = "Offset: " + record.kafkaOffset();
+        String key;
+        if (record.key() == null) {
+            key = "NULL Key";
+        } else {
+            key = "Key: " + keyFormatter.toString(record.key());
+        }
+        String timestamp = "Timestamp: ";
+        System.out.println(record);
+
+        switch (record.timestampType()) {
+            case NO_TIMESTAMP_TYPE:
+                timestamp += "No Timestamp";
+                break;
+            case CREATE_TIME:
+                timestamp += "(create)";
+                timestamp += timestampFormat.format(record.timestamp());
+                break;
+            case LOG_APPEND_TIME:
+                timestamp += "(log append)";
+                timestamp += timestampFormat.format(record.timestamp());
+                break;
+        }
+        String data_length;
+        if (record.value() == null) {
+            data_length = "NULL Value";
+        } else {
+            data_length = "Data: " + valueFormatter.toString(record.value());
+        }
+
+        outputStream.println(offset + " " + key + " " + timestamp + " " + data_length);
     }
 }
